7a45345aacb758157ce6fb5cac8b8f88729f606a
benm4 <144062371+benm4@users.noreply.github.com>
2023-11-13 19:37:25 -0700

    Add files via upload

---
+++ homework 9/Pineapple_snap_2023_11_04_04_25_10/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(227, 218, 201);
+  circle(200, 99, 165);
+  strokeWeight(10);
+  fill(222, 184, 135);
+  point(160, 75);
+  point(250, 75);
+
+  point(200, 95);
+  
+fill(250, 160, 160);
+  ellipse(200, 133, 30, 25);
+  
+  fill(0);
+    rect(150,185,100,150);
+  
+  fill(2, 24, 120);
+    rect(255,244,50,10);
+  
+  rect(110,244,50,10)
+  
+   rect(155,335,10,50);
+  rect(235,335,10,50);
+  
+  
+   fill(120);
+    textSize(12);
+    text("benjamin mccrea",22,389);
+}
